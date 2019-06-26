10ed7b383bff664a996b072bc929a9ec964c8a20
bblake <bblake@goempyrean.com>
2019-06-25 20:28:41 -0500

    build

--- docs/src.a2b27638.js
+++ docs/src.a2b27638.js
@@ -1474,24 +1474,22 @@ var App = function App(mdl) {
       return (0, _mithril.default)(".app", [mdl.isLoading && IsLoading, (0, _mithril.default)("a-scene", [(0, _mithril.default)("a-box", {
         position: "-1 0.5 -3",
         rotation: "0 45 0",
-        color: "#4CC3D9"
+        color: "#003863"
       }), (0, _mithril.default)("a-sphere", {
         position: "0 1.25 -5",
         radius: "1.25",
-        color: "#EF2D5E"
+        color: "#16a085"
       }), (0, _mithril.default)("a-cylinder", {
         position: "1 0.75 -3",
         radius: 0.5,
         height: 1.5,
-        color: "#FFC65D"
+        color: "#d35400"
       }), (0, _mithril.default)("a-plane", {
         position: "0 0 -4",
         rotation: "-90 0 0",
         width: 4,
         height: 4,
-        color: "#7BC8A4"
-      }), (0, _mithril.default)("a-sky", {
-        color: "#003863"
+        color: "#8e44ad"
       })])]);
     }
   };
@@ -1546,7 +1544,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "58434" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "53807" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
--- src/App.js
+++ src/App.js
@@ -22,27 +22,26 @@ const App = (mdl) => {
           m("a-box", {
             position: "-1 0.5 -3",
             rotation: "0 45 0",
-            color: "#4CC3D9"
+            color: "#003863"
           }),
           m("a-sphere", {
             position: "0 1.25 -5",
             radius: "1.25",
-            color: "#EF2D5E"
+            color: "#16a085"
           }),
           m("a-cylinder", {
             position: "1 0.75 -3",
             radius: 0.5,
             height: 1.5,
-            color: "#FFC65D"
+            color: "#d35400"
           }),
           m("a-plane", {
             position: "0 0 -4",
             rotation: "-90 0 0",
             width: 4,
             height: 4,
-            color: "#7BC8A4"
-          }),
-          m("a-sky", { color: "#003863" })
+            color: "#8e44ad"
+          })
         ])
       ])
   }
